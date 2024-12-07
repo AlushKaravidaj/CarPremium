fe8ac82ddf3bb82884b2aa62d723f889ba1d4806
rionkomoni <rionk2005@gmail.com>
2024-12-07 18:10:13 +0100

    Commit js

---
+++ LOGIN PAGE/LoginValidate.js
@@ -0,0 +1,24 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username === "") {
+        alert("Username is required.");
+        event.preventDefault();
+        return false;
+    }
+
+    if (password === "") {
+        alert("Password is required.");
+        event.preventDefault();
+        return false;
+    }
+
+    if (password.length < 6) {
+        alert("Password must be at least 6 characters long.");
+        event.preventDefault();
+        return false;
+    }
+
+    return true;
+});
